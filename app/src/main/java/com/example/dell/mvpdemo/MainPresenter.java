ddd1bf96a6d880db05f25918868e87895bed1a86
Archanakarthi40 <archanakarthi40@gmail.com>
2019-01-03 11:45:12 +0530

    first commit

---
+++ app/src/main/java/com/example/dell/mvpdemo/MainPresenter.java
@@ -0,0 +1,8 @@
+package com.example.dell.mvpdemo;
+
+public interface MainPresenter {
+
+    void onButtonClick();
+
+    void onDestroy();
+}
---
+++ app/src/main/java/com/example/dell/mvpdemo/MainView.java
@@ -0,0 +1,10 @@
+package com.example.dell.mvpdemo;
+
+public interface MainView {
+
+    void showProgress();
+
+    void hideProgress();
+
+    void setQuote(String string);
+}
